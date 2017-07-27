42be016927638136ea0eb0402d1b2de32a1adcfc
Sean <sean.corbett@umontana.edu>
2017-07-26 19:56:52 -0600

    added GET all and POST one routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const Superhero = require('./models/Superhero')
+
 const app = express()
 let port = 3000
 
@@ -9,6 +11,41 @@ mongoose.connect('mongodb://localhost/superheroes')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// GET ALL SUPERHEROES
+app.get('/api', (req,res) => {
+  Superhero.find((err, superheroes) => {
+    if(err){
+      res.json({message: err, data: null})
+    }
+    else{
+      console.log("Now me!")
+      res.json({message: `Successfully retrieved all heroes.`, data: superheroes})
+    }
+  })
+  console.log("No, now me!")
+})
+
+
+
+// POST NEW HERO
+app.post('/api', (req,res) => {
+
+  // Create a new hero via Superhero constructor
+  let newHero = new Superhero()
+
+  newHero.name = req.body.name
+  newHero.superpower = req.body.superpower
+  newHero.image = req.body.image
+
+  newHero.save((err, newHero) => {
+    if(err){
+      res.json({message: err, data: null})
+    }
+    else{
+      res.json({message: `Successfully created new hero: ${newHero.name}`, data: newHero})
+    }
+  })
+})
 
 
 const server = app.listen(port, () => console.log(`Listening on port: ${port}`))
