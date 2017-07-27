130bcac0d17797db341667aacab6505fd22d058b
Sean <sean.corbett@umontana.edu>
2017-07-26 21:07:41 -0600

    added PUT and DELETE

--- server.js
+++ server.js
@@ -43,9 +43,9 @@ app.post('/api', (req,res) => {
   // Create a new hero via Superhero constructor
   let newHero = new Superhero()
 
-  newHero.name = req.body.name
-  newHero.superpower = req.body.superpower
-  newHero.image = req.body.image
+  newHero.name = req.body.name;
+  newHero.superpower = req.body.superpower;
+  newHero.image = req.body.image;
 
   newHero.save((err, newHero) => {
     if(err){
@@ -57,5 +57,43 @@ app.post('/api', (req,res) => {
   })
 })
 
+
+// PUT SINGLE HERO
+app.put('/api/:hero_id', (req,res) => {
+
+  // Find our hero to update
+  Superhero.findById(req.params.hero_id, (err, superhero) => {
+    
+    // If new data then change value, else keep the same
+    superhero.name = (req.body.name) ? req.body.name : superhero.name;
+    superhero.superpower = (req.body.superpower) ? req.body.superpower : superhero.superpower;
+    superhero.image = (req.body.image) ? req.body.image : superhero.image;
+    console.log()
+
+    superhero.save((err, superhero) => {
+      if(err){
+        res.json({message: err, data: null})
+      }
+      else{
+        res.json({message: `Successfully updated hero: ${superhero.name}`, data: superhero})
+      }
+    })
+  })
+})
+
+
+// DELETE SINGLE RECORD
+app.delete('/api/:hero_id', (req,res) => {
+
+  // Find hero to delete via search object
+  Superhero.remove({_id: req.params.hero_id}, (err) => {
+    if(err){
+      res.send({message: err, data: null})
+    }
+    else{
+      res.send({message: `Superhero successfully deleted!`, data: {}})
+    }
+  })
+})
 
 const server = app.listen(port, () => console.log(`Listening on port: ${port}`))
