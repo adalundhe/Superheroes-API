1898d918ec93b8f07a4fdde75487c6733b222d37
Sean <sean.corbett@umontana.edu>
2017-08-02 19:18:44 -0600

    added

--- public/hero.js
+++ public/hero.js
@@ -9,23 +9,26 @@ const app = new Vue({
     heroFormTitle: heroFormTitle,
     name: '',
     superpower: '',
-    image: ''
+    image: '',
+    selectedHero: undefined,
+    heroIndex: undefined,
+    showEdit: false
   },
   created(){
     this.loadData()
   },
   methods: {
-    loadData: function(){
-      let self = this
+    loadData(){
+      const self = this
 
       $.ajax({
         url: '/api/superheroes',
         method: 'GET'
       }).done((response) => self.heroes = response.data)
     },
-    createHero: function(){
-      let self = this
-      let newHero = {
+    createHero(){
+      const self = this
+      const newHero = {
                       name: this.name,
                       superpower: this.superpower,
                       image: this.image
@@ -36,6 +39,37 @@ const app = new Vue({
         method: 'POST',
         data: newHero
       }).done((response) => console.log(response))
+    },
+    deleteHero(hero_id){
+      const self = this
+
+      $.ajax({
+        url: `/api/superheroes/${hero_id}`,
+        method: 'DELETE',
+      }).done((response) => self.loadData())
+    },
+    toggleEdit(hero_id, index){
+      this.showEdit = !this.showEdit
+      this.selectedHero = (this.showEdit) ? hero_id : undefined
+      this.heroIndex = (this.showEdit) ? index : undefined
+    },
+    editHero(){
+      const self = this
+
+      const modifiedHero = {
+        name: (self.name.length > 0) ? self.name : null,
+        superpower: (self.superpower.length > 0) ? self.superpower : null,
+        image: (self.image.length > 0) ? self.image : null
+      }
+
+      $.ajax({
+        url: `/api/superheroes/${self.selectedHero}`,
+        method: 'PUT',
+        data: modifiedHero
+      }).done((response) => {
+        self.heroes[self.heroIndex] = response.data
+        self.showEdit = false
+      })
     }
   }
 })
